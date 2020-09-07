f58db5163c291a61fea7092cb30a8180366b33eb
unknown <mo.rahman610@gmail.com>
2020-09-07 10:36:16 -0400

    Drawing Canvas Objects Complete

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.playground
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvas = MyCanvas(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+        myCanvas.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvas)
     }
 }
